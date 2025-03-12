8c8b152ed9ce8462f1154d33b640396dc8b6712d
nsshivu1209 <nsshivu1209@gmail.com>
2025-03-13 00:42:55 +0530

    new branch

--- src/Array/Duplicate.java
+++ src/Array/Duplicate.java
@@ -1,17 +1,17 @@
 package Array;
 
 public class Duplicate {
-public static void main(String[] args) {
-	String s="test yantra";
-	char[]a=s.toCharArray();
-	String s1="";
-	for(int i=0;i<a.length;i++){
-		if(a[i]!=a[i+1]) {
-			s1=s1+a[i];
-			System.out.print(s1);		
-		}
-		
-	}
-	
-}
+    public static void main(String[] args) {
+        String s = "test yantra";
+        char[] a = s.toCharArray();
+        String s1 = "";
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != a[i + 1]) {
+                s1 = s1 + a[i];
+                System.out.print(s1);
+            }
+
+        }
+
+    }
 }
